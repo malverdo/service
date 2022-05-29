03b7e4aeec42c366bf46f8a4c10a6495ed74ac81
malverdo <malverdo11@mail.ru>
2022-05-29 15:09:22 +0300

    token TOKEN_ITEMSCC secret

--- service/src/Client/ItemsccService.php
+++ service/src/Client/ItemsccService.php
@@ -2,23 +2,53 @@
 
 namespace App\Client;
 
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ItemsccService
 {
     private $client;
 
+    public $token;
+
+    /**
+     * @return string
+     */
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     */
+    public function setToken(string $token)
+    {
+        $this->token = $token;
+    }
+
     public function __construct(HttpClientInterface $client)
     {
         $this->client = $client;
     }
 
+    /**
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     */
     public function getItemsOffset(int $offset = 0): ?array
     {
 
         $response = $this->client->request(
             'GET',
-            'https://itemsccservice.com/v2/items/show/csgo?token=199eee3e44c074f7b308e852a02ce92f&offset=' . $offset
+            'https://itemsccservice.com/v2/items/show/csgo?token=' . $this->getToken() . '&offset=' . $offset
         );
 
         if (empty($response->toArray()['items'])) {
