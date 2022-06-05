f0b6e8f3948894665628af96e778d28ad382edb1
malverdo <malverdo11@mail.ru>
2022-06-05 15:22:03 +0300

    elastic

---
+++ service/src/MessageHandler/SmsNotificationHandler.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\MessageHandler;
+
+use App\Message\SmsNotification;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+/**
+ * @AsMessageHandler()
+ */
+class SmsNotificationHandler
+{
+    public function __invoke(SmsNotification $message)
+    {
+        $a = 0;
+    }
+}
