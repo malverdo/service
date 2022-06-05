7de113fca4f402a915ff75a5043e93cf5c381cd9
malverdo <malverdo11@mail.ru>
2022-06-05 15:21:55 +0300

    elastic

---
+++ service/src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Message\SmsNotification;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class DefaultController extends AbstractController
+{
+
+
+    public function index(MessageBusInterface $bus)
+    {
+        // will cause the SmsNotificationHandler to be called
+        $bus->dispatch(new SmsNotification('Look! I creasdated a message!'));
+
+        return new JsonResponse('hello');
+    }
+}
